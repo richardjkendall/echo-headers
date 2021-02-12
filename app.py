4e0d7f539b23eb3ec203359be41379deb3c5fc54
Richard Kendall <richard.kendall@gmail.com>
2021-02-13 06:02:19 +1100

    add support to repond on any path

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
 
-@app.route("/")
-def indexheaders():
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def indexheaders(path):
   # get headers on request
   headers = {}
   for key,value in dict(request.headers).items():
     headers[key] = value
+  headers["Requested-Path"] = path
   # return response
   return jsonify(headers)
